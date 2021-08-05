7b5feeadeaa885e86e10ed08d8c231ad752b9ddb
Jorge Armas <jorgearmas51@gmail.com>
2021-08-04 23:30:23 -0500

    primer commit

---
+++ hbs/helpers.js
@@ -0,0 +1,38 @@
+const hbs=require('hbs');
+
+//Para el Producto1
+hbs.registerHelper('Producto1',(i)=>{
+    let fs=require('fs');
+    let data=fs.readFileSync('./data/data.json', 'utf8');
+    var words=JSON.parse(data);
+    if(i==1){
+        return words[0].Producto1;//Regresa el Nombre del producto 1
+    }else{
+        return words[0].Precio1;//Regresa el precio del producto 1
+    }
+    ;
+});
+
+//Para el Producto2
+hbs.registerHelper('Producto2',(i)=>{
+    let fs=require('fs');
+    let data=fs.readFileSync('./data/data.json', 'utf8');
+    var words=JSON.parse(data);
+    if(i==1){
+        return words[1].Producto2;//Regresa el Nombre del producto 2
+    }else{
+        return words[1].Precio2;//Regresa el precio del producto 2
+    }
+});
+
+//Para el Producto3
+hbs.registerHelper('Producto3',(i)=>{
+    let fs=require('fs');
+    let data=fs.readFileSync('./data/data.json', 'utf8');
+    var words=JSON.parse(data);
+    if(i==1){
+        return words[2].Producto3;//Regresa el Nombre del producto 3
+    }else{
+        return words[2].Precio3;//Regresa el precio del producto 3
+    };
+});
---
+++ server.js
@@ -0,0 +1,20 @@
+const express=require('express');
+const app=express();
+
+const hbs=require('hbs');
+require('./hbs/helpers');
+
+const port=process.env.PORT ||3000;
+hbs.registerPartials(__dirname + '/views/partials');
+
+
+app.use(express.static(__dirname+'/public'));
+app.set('view engine','hbs');
+
+app.get(`/`,(req,res)=>{
+    res.render('home');
+});
+
+app.listen(3000, ()=>{
+    console.log(`Servidor ejecutandose en el puerto ${port}`);
+});
